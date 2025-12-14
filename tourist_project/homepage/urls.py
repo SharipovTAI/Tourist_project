27a138226b63472bcf5d1e5798d6728a61c0b25d
SharipovTAI <sharipovtimur200666@gmail.com>
2025-12-14 18:59:04 +0500

    Update urls.py
    Update urls.py

--- tourist_project/homepage/urls.py
+++ tourist_project/homepage/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('ask-llama/', views.ask_llama, name='ask_llama'),
+    path('debug_search/', views.debug_search, name='debug_search'),
 ]
